eeb944e2f49fe5eca5f547833f75b65e70f03e38
OmarSagazio <omar.sagazio@gmail.com>
2017-09-28 17:49:28 +0200

    chat

---
+++ fullstack/chat/public/actions/index.js
@@ -0,0 +1,19 @@
+export const ADD_MESSAGE = 'ADD_MESSAGE';
+export const DELETE_MESSAGE = 'DELETE_MESSAGE';
+
+
+export const addMessageAction1 = {
+    type: ADD_MESSAGE,
+    message: 'How does it look, Neil?'
+};
+
+export const addMessageAction2 = {
+    type: ADD_MESSAGE,
+    message: 'Looking good.'
+};
+
+
+export const deleteMessageAction = {
+    type: DELETE_MESSAGE,
+    index: 0
+};
--- fullstack/chat/src/index.js
+++ fullstack/chat/src/index.js
@@ -4,7 +4,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import MainComponent from "../public/components/MainComponent";
+import { reducer } from "../public/reducers/index";
+import { addMessageAction1, addMessageAction2, deleteMessageAction } from "../public/actions/index";
 
+
+const initialState = {
+    messages: []
+};
+
+const createStore = (reducer, initialState) => {
+    let state = initialState;
+    const listeners = [];
+
+    const subscribe = listener => listeners.push(listener);
+
+
+    const getState = () => state;
+
+    const dispatch = action => {
+        state = reducer(state, action);
+    };
+
+    return {
+        subscribe,
+        getState,
+        dispatch
+    }
+
+};
+
+
+const store = createStore(reducer, initialState);
+
+
+store.dispatch(addMessageAction1);
+const stateV1 = store.getState();
+console.log(stateV1);
+
+store.dispatch(addMessageAction2);
+const stateV2 = store.getState();
+console.log(stateV2);
+
+
+store.dispatch(deleteMessageAction);
+const stateV3 = store.getState();
+console.log(stateV3);
 
 ReactDOM.render(
     <MainComponent/>,
